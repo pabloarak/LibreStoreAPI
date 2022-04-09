2263f2ed7cd52321b4a98d81b172b2a960c992f5
Pablo Arak <pablo.arak.vm@gmail.com>
2022-04-09 11:24:58 -0400

    Added boom configuration

--- routes/products.router.js
+++ routes/products.router.js
@@ -30,11 +30,15 @@ router.post('/', async (req, res) => {
   res.status(201).json(newProduct);
 });
 
-router.patch('/:id', async (req, res) => {
-  const { id } = req.params;
-  const body = req.body;
-  const product = await service.update(id, body);
-  res.json(product);
+router.patch('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const body = req.body;
+    const product = await service.update(id, body);
+    res.json(product);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
