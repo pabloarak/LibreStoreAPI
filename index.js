68e1ac3612fa7f512b1b53a33c4206672a12df6b
Pablo Arak <pablo.arak.vm@gmail.com>
2022-04-09 10:10:52 -0400

    Added first middlewares for error handlers

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const routerApi = require('./routes');
 
+const { logErrors, errorHandler } = require('./middlewares/error.handler');
+
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
 routerApi(app);
+
+app.use(logErrors);
+app.use(errorHandler);
+
 
 app.listen(port, () => {
   console.log('Start in port ' +  port);
--- routes/products.router.js
+++ routes/products.router.js
@@ -5,8 +5,8 @@ const ProductsService = require('./../services/product.service');
 const router = express.Router();
 const service = new ProductsService();
 
-router.get('/', (req, res) => {
-  const products = service.find();
+router.get('/', async (req, res) => {
+  const products = await service.find();
   res.json(products);
 });
 
@@ -14,28 +14,32 @@ router.get('/filter', (req, res) => {
   res.send('Yo soy un filter');
 });
 
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
-  const product = service.findOne(id);
-  res.json(product);
+router.get('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const product = await service.findOne(id);
+    res.json(product);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const body = req.body;
-  const newProduct = service.create(body);
+  const newProduct = await service.create(body);
   res.status(201).json(newProduct);
 });
 
-router.patch('/:id', (req, res) => {
+router.patch('/:id', async (req, res) => {
   const { id } = req.params;
   const body = req.body;
-  const product = service.update(id, body);
+  const product = await service.update(id, body);
   res.json(product);
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   const { id } = req.params;
-  const response = service.delete(id);
+  const response = await service.delete(id);
   res.json(response);
 });
 
