defaba5041049f4fe1209c94b200813d1e9072e5
Pablo Arak <pablo.arak.vm@gmail.com>
2022-04-10 13:43:15 -0400

    Added new error handler for sequelize

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routerApi = require('./routes');
 const cors = require('cors');
 
-const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
+const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
 const port = 3000;
@@ -22,6 +22,7 @@ const options = {
 app.use(cors(options));
 
 routerApi(app);
+app.use(ormErrorHandler);
 app.use(logErrors);
 app.use(boomErrorHandler);
 app.use(errorHandler);
--- middlewares/error.handler.js
+++ middlewares/error.handler.js
@@ -1,11 +1,11 @@
+const { ValidationError } = require('sequelize');
+
 function logErrors(err, req, res, next) {
-  console.log('logErrors');
   console.error(err);
   next(err);
 }
 
 function errorHandler(err, req, res, next) {
-  console.log('errorHandler');
   res.status(500).json({
     message: err.message,
     stack: err.stack,
@@ -20,4 +20,15 @@ function boomErrorHandler(err, req, res, next) {
   next(err);
 }
 
-module.exports = { logErrors, errorHandler, boomErrorHandler };
+function ormErrorHandler(err, req, res, next) {
+  if (err instanceof ValidationError) {
+    res.status(409).json({
+      statusCode: 409,
+      message: err.name,
+      errors: err.errors
+    });
+  }
+  next(err);
+}
+
+module.exports = { logErrors, errorHandler, boomErrorHandler, ormErrorHandler };
