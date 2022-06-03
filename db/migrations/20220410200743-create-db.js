8d5d4d008fcf8afdbeb2ada7f4cba0fc747310ff
Pablo Arak <pablo.arak.vm@gmail.com>
2022-06-03 17:02:18 -0400

    Fix order model

--- db/migrations/20220410200743-create-db.js
+++ db/migrations/20220410200743-create-db.js
@@ -5,7 +5,7 @@ const { USER_TABLE, UserSchema } = require('../models/user.model');
 const { CUSTOMER_TABLE, CustomerSchema } = require('../models/customer.model');
 const { CATEGORY_TABLE, CategorySchema } = require('../models/category.model');
 const { PRODUCT_TABLE, ProductSchema } = require('../models/product.model');
-const { ORDER_TABLE, OrderSchema } = require('../models/order.model');
+const { ORDER_TABLE } = require('../models/order.model');
 const {
   OrderProductSchema,
   ORDER_PRODUCT_TABLE,
@@ -17,7 +17,31 @@ module.exports = {
     await queryInterface.createTable(CUSTOMER_TABLE, CustomerSchema);
     await queryInterface.createTable(CATEGORY_TABLE, CategorySchema);
     await queryInterface.createTable(PRODUCT_TABLE, ProductSchema);
-    await queryInterface.createTable(ORDER_TABLE, OrderSchema);
+    await queryInterface.createTable(ORDER_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      customerId: {
+        field: 'customer_id',
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: CUSTOMER_TABLE,
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW,
+      },
+    });
     await queryInterface.createTable(ORDER_PRODUCT_TABLE, OrderProductSchema);
   },
 
